75522f01dd3630cbcd368827702b1230004d94ab
Nagaraj Vandal <nagarajvandal@gmail.com>
2019-01-13 12:20:16 +0530

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,116 @@
+$(window).on("load", function() {
+    $(".loader .inner").fadeOut(500, function(){
+        $(".loader").fadeOut(750);
+    });
+})
+
+$(window).on("load" ,function(){
+    $(".items").isotope({
+        filter:'*',
+        animationOptions:{
+            duration:1500,
+            easing:'linear',
+            queue:false
+        }
+    });
+})
+
+$(document).ready(function(){
+    
+    $('#slides').superslides({
+        animation : "fade",
+        play: 5000 ,
+        pagination : false
+    });
+
+    var typed = new Typed(".typed", {
+        strings : ["Web Developer by Day." , "Gamer by Night!"],
+        typeSpeed : 70,
+        loop:true,
+        startDelay : 1000,
+        showCursor : false
+    });
+
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        items : 4,
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            938:{
+                items:4
+            }
+        }
+    });
+
+    var skillsPosition = $(".skillsection").offset().top;
+    var statsPosition = $(".statsection").offset().top;
+    var countupFinished = false;
+    $(window).scroll(function() {
+        if( pageYOffset > skillsPosition - $(window).height() + 280){
+            $('.chart').easyPieChart({
+                easing : "easeInOut",
+                barColor :'#fff',
+                trackColor : "#000",
+                scaleColor : false,
+                lineWidth : 4,
+                size : 152,
+                onStep : function(from , to ,percent){ 
+                    $(this.el).find(".percent").text(Math.round(percent));
+                }
+            });
+        }
+
+        if(!countupFinished && pageYOffset > statsPosition - $(window).height() + 280){
+            $(".counter").each(function(){
+                var element = $(this);
+                var endVal = parseInt(element.text());
+                element.countup(endVal)
+            });
+
+            countupFinished = true
+        }
+    });
+
+   $("[data-fancybox]").fancybox();
+  
+   
+    $("#filters a").click(function(){
+        $("#filters .current").removeClass("current");
+        $(this).addClass("current");
+
+        var selector = $(this).attr("data-filter");
+        $(".items").isotope({
+            filter:selector,
+            animationOptions:{
+                duration:1500,
+                easing:'linear',
+                queue:false
+            }
+        });
+        return false;
+
+    })
+
+    $("#navigation li a ").click(function(e) {
+        e.preventDefault();
+        var targetElement = $(this).attr("href");
+        var targetPosition = $(targetElement).offset().top;
+        $("html , body").animate({
+            scrollTop: targetPosition - 50
+        },"slow")
+    })
+   
+   
+    
+});
+
+
